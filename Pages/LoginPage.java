246e7ea35944a2097a4b1a27be736451d2869ce0
Umamahesh835 <umamumr@gmail.com>
2026-02-22 14:01:30 +0530

    LoginPage

---
+++ Pages/LoginPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+import Base.BasePage;
+
+public class LoginPage extends BasePage{
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        super(driver);
+    }
+    
+}
